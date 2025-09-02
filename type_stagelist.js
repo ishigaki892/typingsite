00765e10349b0920e115d1ecb13154f0e7629724
ishigaki892 <kazuma.kakoi@gmail.com>
2025-09-02 19:22:25 +0900

    Add files via upload

---
+++ type_stagelist.js
@@ -0,0 +1,30 @@
+let jp1 = {
+  title: "日本編",
+  def: "1章",
+  name: ["わんこ", "にょろ", "例のやつ", "カバちゃん", "ブタヤロウ", "ジャッキー・ペン", "ゴリさん", "メェメェ", "ゴマさま", "パオン", "カ・ンガリュ", "一角くん", "クマ先生", "ワニック", "ウサ銀", "リッスントゥミー", "カオル君"],
+  hira: ["わんこ", "にょろ", "れいのやつ", "かばちゃん", "ぶたやろう", "じゃっきー・ぺん", "ごりさん", "めぇめぇ", "ごまさま", "ぱおん", "か・んがりゅ", "いっかくくん", "くませんせい", "わにっく", "うさぎん", "りっすんとぅみー", "かおるくん"],
+  sets: [false, false, false]
+};
+
+let dbg = {
+  title: "デバック用",
+  def: "管理者用",
+  name: ["1234567890", "1234567890"],
+  hira: ["1234567890", "1234567890"],
+  sets: [false, false, false]
+};
+let img = {
+  title: "画像でGO",
+  def: "神ムズ",
+  name: ["<img src='./img/muteki.png'>", "<img src='./img/megidora.png'>"],
+  hira: ["ごくむてきかんたいしーがれおん", "せいりゅうこうていめぎどら"],
+  sets: [false, true, true]
+}
+let superhavi = {
+  title: "haviまみれ",
+  def: "senムズ",
+  name: ["havi","havi","havi","havi","havi","havi","havi","havi","havi","havi","havi","havi","havi","havi","havi","havi","havi","havi","havi","havi","havi","havi","havi","havi","havi","havi","havi","havi","havi","havi","havi","havi","havi","havi"],
+  hira: ["はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ","はヴぃ"],
+  sets: [false, false, false]
+}
+export {jp1, dbg, img, superhavi};
